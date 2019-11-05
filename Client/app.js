e1524f910f83e5d4a1644d29fa1ad6adc39e9c9a
gabrielkunkel <henrysaltandson@gmail.com>
2019-11-05 09:39:50 -0600

    populate update form with click on tr

--- Client/app.js
+++ Client/app.js
@@ -58,37 +58,24 @@ var idForRecordToUpdate;
                 console.log(data);
 
                 allData.forEach( item => {
-                    let info = item.Title+item.DirectorName+item.Genre;
                     
-                    $('#results').append('<tr id=\"m' + item.MovieId + '\" class=\"movieList\"><td>' + item.Title +'</td><td>' + item.DirectorName +'</td><td>' + item.Genre +'</td></tr>');
+                    $('#results')
+                        .append('<tr id=\"m' + item.MovieId + '\" class=\"movieList\"><td>' + item.Title +'</td><td>' + item.DirectorName +'</td><td>' + item.Genre +'</td></tr>')
+                        .on("click", '#m' + item.MovieId, function () {
+                            $('#title-input').val(item.Title);
+                            $('#director-input').val(item.DirectorName);
+                            $('#genre-input').val(item.Genre);
+                         });
                 })
 
                 console.log("done populating");
-
-               // $('#response pre').html( data );
-
-                // store the records with Ids
-
-                // make the records clickable: each time you click on a particular record it populates the update form
-                    // fill up array
-                    // filter with the the particular one that we clicked
-
-                // On submit update record
 
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
             
-        }).done(function() {
-            allData.forEach(function (item) {
-                $('\"#m' + item.MovieId + '\"').click(function () {
-                    $('#title-input').val(d.Title);
-                    $('#director-input').val(d.Title);
-                    $('#genre-input').val(d.Title);
-                 });
-            });
-        }); 
+        }) 
 
         e.preventDefault();
     }
